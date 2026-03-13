0294f035f4fb91f495bbd46da4636960cd3658aa
harshabandaru2410 <harshabandaru2410@gmail.com>
2026-03-13 14:47:33 +0530

    UC1: Add welcome message module

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,11 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to BookMyStay ");
+        System.out.println(" Hotel Booking System v1.0 ");
+        System.out.println("=================================");
+
+    }
+}
